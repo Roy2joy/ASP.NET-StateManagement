9c427a4c431693d65f406a3401ef21ccc0c8f397
Nabeel <khankumail298@gmail.com>
2021-08-20 03:34:09 +0500

    adding asp.net session package and setting base file

---
+++ Controllers/SessionController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StateProj.Controllers
+{
+    public class SessionController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Test(string name, string pwd)
+        {
+            if (name == "abc" && pwd == "123")
+            {
+
+
+                return RedirectToAction("Display");
+            }
+            else
+            {
+                return RedirectToAction("Index");
+            }
+        }
+
+        public IActionResult Display()
+        {
+            if (/*Request.Cookies["userName"] != null*/true)
+            {
+                //string cryptext = Request.Cookies["userName"];
+                //string result = Decrypt(cryptext);
+                //ViewBag.temp = result;
+            }
+            else
+            {
+                //ViewBag.temp = "anonymous";
+            }
+            return View();
+        }
+
+        public IActionResult DeleteCookie()
+        {
+            if (Request.Cookies["userName"] != null)
+            {
+                Response.Cookies.Delete("userName");
+            }
+            return RedirectToAction("Display");
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -50,7 +50,7 @@ namespace StateProj
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Session}/{action=Index}/{id?}");
             });
         }
     }

6fd1ebdd7f7e872a8ade35e26b7704765bd440c2
Nabeel <khankumail298@gmail.com>
2021-08-20 03:44:43 +0500

    adding user model(not necessary to add but i want to use model with session and explore way to store object with session)

---
+++ Models/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StateProj.Models
+{
+    public class User
+    {
+        public String name  { get; set; }
+        public String pwd   { get; set; }
+    }
+}

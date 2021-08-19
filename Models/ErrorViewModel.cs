3778fb575c11edc5ad10724cfcc1f09242e6fd0c
Nabeel <khankumail298@gmail.com>
2021-08-19 21:40:10 +0500

    experimenting with hidden fields

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace StateProj.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
